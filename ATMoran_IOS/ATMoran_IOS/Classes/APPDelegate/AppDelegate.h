ca4bb92a4348d3ee72041d7fe0811bfd332ec1f5
AntsTower <Ants@AntsdeMac-mini.local>
2015-10-13 14:20:36 +0800

    Create App
    create app project

---
+++ ATMoran_IOS/ATMoran_IOS/Classes/APPDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATMoran_IOS
+//
+//  Created by AntsTower on 15/10/13.
+//  Copyright © 2015年 Ants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ATMoran_IOS/ATMoran_IOS/Classes/APPDelegate/AppDelegate.m
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.m
+//  ATMoran_IOS
+//
+//  Created by AntsTower on 15/10/13.
+//  Copyright © 2015年 Ants. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+   
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+   
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+   
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+}
+
+@end
